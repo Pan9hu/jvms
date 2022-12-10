a436f62e9ab5d2962409cd3f83fb3efd022434b3
Pan9Hu <11740305+pan9hu@user.noreply.gitee.com>
2022-12-10 16:36:06 +0800

    update 2022-12-10 4:50

--- repository-server-template/pull_repo.sh
+++ repository-server-template/pull_repo.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env sh
 service nginx start && service cron start
 cd /var/www/html && git clone https://github.com/bluemiaomiao/jjvmm-repo.git
+cat > /etc/nginx/sites-enabled/default << EOF
+server{
+	server_name _;
+	listen 80;
+	root /var/www/html;
+	location / {
+		autoindex on;
+	}
+}
+EOF
 echo '*/5 * * * * root cd /var/www/html && git clone https://github.com/bluemiaomiao/jjvmm-repo.git' >> /etc/crontab
 tail -f /dev/null
